3a2115ecf59710c42f2742cdc2d39b1fafdceb9f
vishalmahle11 <vishalmahale974@gmail.com>
2022-09-14 11:54:07 +0530

    Add skills Section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -70,11 +70,7 @@ export function HeroSection() {
         </div>
 
         <div className={styles.photoCont}>
-          <img
-            src="https://camo.githubusercontent.com/e4a569755580f96dce0e6d65bc761e0d9aef0fecae524ec73a1b0be60fc934fa/68747470733a2f2f7777772e6d79676f2e67652f75706c6f6164732f626c6f672f313538343032333739352e6a7067"
-            alt=""
-            className={styles.heroImg}
-          />
+          <img src="/Vishal_pic.png" alt="logo" className={styles.heroImg} />
         </div>
         <div className={styles.socialCont}>
           <a
